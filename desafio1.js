04a05f8c65d177683f30d6ba45816348ca5942fe
Felipe Rímola <feliperimola9@gmail.com>
2023-02-23 05:10:43 -0300

    reentrega 1 y entrega desafio 2

--- desafio1.js
+++
@@ -1,7 +0,0 @@
-const contenedor = new Contenedor()
-contenedor.agregar(new Producto({nombre: 'lapiz', precio: '150', code: '01', stock: '14'}))
-contenedor.agregar(new Producto({nombre: 'lapicera', precio: '300', code: '02', stock: '28'}))
-contenedor.agregar(new Producto({nombre: 'tijera', precio: '500', code: '03', stock: '32'}))
-contenedor.agregar(new Producto({nombre: 'goma', precio: '200', code: '04', stock: '20'}))
-console.log(contenedor.obtenerTodo());
-console.log(contenedor.obtenerLapiz())
