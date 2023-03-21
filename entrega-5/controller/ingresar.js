ae6714f8a8371dfe97c80e9b6dec106056f2a7bf
Felipe Rímola <feliperimola9@gmail.com>
2023-03-21 17:01:44 -0300

    entrega websocket

---
+++ entrega-5/controller/ingresar.js
@@ -0,0 +1,11 @@
+const { response, request } = require('express') 
+const {productos} = require('../helpers/almacenarProductos')
+
+const ingresarProducto = (req,res=response)=>{
+    res.render('ingresar',{productos})
+}
+
+module.exports = {
+    ingresarProducto
+}
+ 
